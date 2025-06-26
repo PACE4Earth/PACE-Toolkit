a344ac479261ee7b87670b19aaf7d444a73416d5
vozar2 <vozar2@jwlogin01.juwels>
2025-06-26 11:03:59 +0200

    rename sbatch out files

--- testrun.sh
+++ testrun.sh
@@ -2,8 +2,8 @@
 #SBATCH --account=hclimrep
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --output=mpi-out.%j
-#SBATCH --error=mpi-err.%j
+#SBATCH --output=job_output.%j
+#SBATCH --error=job_error.%j
 #SBATCH --time=00:05:00
 
 module load Stages/2025 GCCcore/.13.3.0 SciPy-Stack/2024a PyTorch/2.5.1 netcdf4-python/1.7.1.post2-serial
