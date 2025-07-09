7d9f26b99a5985a7a8ab99db961dd5d3980e007a
vozar2 <vozar2@jwlogin09.juwels>
2025-07-09 12:54:13 +0200

    add testrun.sh

---
+++ testrun.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --account=hclimrep
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --output=job_output.%j
+#SBATCH --error=job_error.%j
+#SBATCH --time=00:05:00
+
+module load Stages/2025 GCCcore/.13.3.0 SciPy-Stack/2024a PyTorch/2.5.1 netcdf4-python/1.7.1.post2-serial
+
+srun python ./pace/evaluator.py
