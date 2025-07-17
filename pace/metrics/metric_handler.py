02b5312c075df7d4a4da65e1724f76fe7b102d95
vozar2 <vozar2@jwlogin06.juwels>
2025-07-17 16:29:54 +0200

    add metric_handler wrapper

---
+++ pace/metrics/metric_handler.py
@@ -0,0 +1,26 @@
+import torch
+from torch import nn
+
+from geostrophic_new import GeostrophicWind
+
+METRIC_MODULES = {
+    'geostrophic_balance' : GeostrophicWind()
+} 
+
+class MetricHandler(nn.Module):
+    def __init__(self, grid, metrics: list[str]):
+        super().__init__()
+        
+        self.grid = grid
+        
+        self.metrics = {metric: METRIC_MODULES[metric] for metric in metrics}
+
+    def forward(self, sample):
+        
+        outputs = {}
+        
+        for metric, module in self.metrics:
+            outputs[metric] = module(sample)
+        
+        return outputs
+    
