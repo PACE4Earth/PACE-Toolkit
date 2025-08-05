5a66a224e824b64763fba8e397ed70e1a4be8ec9
vozar2 <vozar2@jwlogin05.juwels>
2025-08-05 15:06:41 +0200

    add inspect_cordiff troubleshooting script

---
+++ dirty_bit/inspect_corrdiff.py
@@ -0,0 +1,36 @@
+import os
+import ast
+import json
+import xarray as xr
+
+base_path = '/p/scratch/hclimrep/pavel1/CorrDiff/CorrDiff_Output_Code4Earth'
+
+files = [
+    os.path.join(base_path, f)
+    for f in os.listdir(base_path)
+    if '.nc' in f
+]
+
+files.sort()
+
+# this_path = files[-1]
+this_path = '/p/scratch/hclimrep/pavel1/CorrDiff/crea2_6h/Output/Output_allVars/corrdiff_output_ensemble_6h_8.nc'
+
+with xr.open_dataset(this_path, engine='netcdf4') as ds:
+    # print(ds)
+    cfg = ast.literal_eval(ds.attrs['cfg'])
+    # for k, v in cfg.items():
+    #     print(k, v)
+    #     print()
+    da = ds.isel(time=0).values
+    print(da)    
+#     output_filename = cfg['generation']['io']['output_filename']
+    
+# try:
+#     with xr.open_dataset(output_filename, engine='netcdf4') as ds:
+#         print(ds)
+# except Exception as e:
+#     print(e)    
+
+        
+    
